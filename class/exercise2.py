1407818686429a601ee3d8976fb2e2da0639755c
VladHornit <hornitskyy_vlad@ukr.net>
2022-08-18 20:26:36 +0200

    modified exercise2

--- class/exercise2.py
+++ class/exercise2.py
@@ -4,8 +4,8 @@ class Citizen:
         self.trip = []
         self.diet = diet
 
-    def compute_carbon_foorprint(self, diet_carbon_footprint, house_footprint, trip_footprint):
-        human_carbon_footprint = result_diet_carbon_footprint + house_footprint + trip_footprint
+    def compute_carbon_footprint(self):
+        human_carbon_footprint =  self.house.compute_house_footprint + self.trip.compute_trip_footprint + self.diet.compute_house_footprint
         return human_carbon_footprint
 
 class Diet:
@@ -13,13 +13,13 @@ class Diet:
         self.diet_type = diet_type
         self.distance_from_farm = distance_from_farm
 
-    def compute_diet_carbon_footprint(self, diet_type, distance_from_farm):
+    def compute_diet_carbon_footprint(self):
         diet_footprint = 1000 * (self.distance_from_farm / 100)
-        if diet_type == "vegetarian":
-            result_diet_carbon_footprint = 5 * diet_foorprint / 6
+        if self.diet_type == "vegetarian":
+            diet_carbon_footprint = 5 * diet_footprint / 6
         else:
-            result_diet_carbon_footprint = 3 * diet_foorprint / 4
-        return result_diet_carbon_footprint
+            diet_carbon_footprint = 3 * diet_footprint / 4
+        return diet_carbon_footprint
 
 class House:
     def __init__(self, flat, size, heating_temperature):
@@ -28,9 +28,9 @@ class House:
         self.heating_temperature = heating_temperature
 
 
-    def compute_house_footprint(self, flat, size, heating_temperature):
-        footprint = (heating_temperature - 17) * 10 * self.size
-        if flat == "flat":
+    def compute_house_footprint(self):
+        footprint = (self.heating_temperature - 17) * 10 * self.size
+        if self.flat == "flat":
             house_footprint = 2 * footprint / 3
         return house_footprint
 
@@ -40,10 +40,10 @@ class Trip:
         self.type = type
         self.distance = distance
 
-    def compute_trip_footprint(self, type, distance):
-        if type == "plane":
+    def compute_trip_footprint(self):
+        if self.type == "plane":
             trip_footprint = (self.distance / 100) * 10
-        elif type == "car":
+        elif self.type == "car":
             trip_footprint = (self.distance / 100) * 8
         else:
             trip_footprint = (self.distance / 100) * 0.2
@@ -51,16 +51,35 @@ class Trip:
 
 
 
-diet_citizen_1 = Diet("organik", 100)
+diet_1 = Diet("organik", 100)
 flat_1 = House("flat", 50, 20)
 trip_1 = Trip("plane", 2500)
-citizen_1 = Citizen(flat_1, trip_1, diet_citizen_1)
+citizen_1 = Citizen(flat_1, trip_1, diet_1)
 
-diet_citizen_1.compute_diet_carbon_footprint
-flat_1.compute_house_footprint
-trip_1.compute_trip_footprint
-citizen_1.compute_carbon_foorprint
-print(citizen_1.compute_carbon_foorprint)
+print(diet_1.compute_diet_carbon_footprint())
+print(flat_1.compute_house_footprint())
+print(trip_1.compute_trip_footprint())
+
+print(citizen_1.house)
+print(citizen_1.compute_carbon_footprint())
+
+#print(diet_1.compute_diet_carbon_footprint("organik", 100))
+#print(flat_1.compute_house_footprint("flat", 50, 20))
+#print(trip_1.compute_trip_footprint("plane", 2500))
+
+#print(citizen_1.diet)
+
+
+#diet_citizen_1.compute_diet_carbon_footprint()
+#citizen_1.compute_carbon_foorprint()
+#trip_1.compute_trip_footprint(trip_1)
+#citizen_1.diet.compute_diet_carbon_footprint()
+
+
+#flat_1.compute_house_footprint()
+#trip_1.compute_trip_footprint()
+#citizen_1.compute_carbon_foorprint()
+#print(citizen_1.compute_carbon_foorprint())
 
 #print(diet_citizen_1.compute_diet_carbon_footprint)
 #print(diet_citizen_1.diet_type)
